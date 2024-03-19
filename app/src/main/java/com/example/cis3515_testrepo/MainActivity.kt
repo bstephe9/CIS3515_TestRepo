6fd37e82874b0003895fc1a3d8abfc22c70dcbc7
bstephe9 <tun78906@temple.edu>
2024-03-18 20:46:16 -0400

    feat(MainActivity): Implement button changing both images

--- app/src/main/java/com/example/cis3515_testrepo/MainActivity.kt
+++ app/src/main/java/com/example/cis3515_testrepo/MainActivity.kt
@@ -2,16 +2,25 @@ package com.example.cis3515_testrepo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val fragment1 = ImageFragment()
+        val fragment2 = ImageFragment()
+
         supportFragmentManager
             .beginTransaction()
-            .add(R.id.imageFragmentContainer1, ImageFragment())
-            .add(R.id.imageFragmentContainer2, ImageFragment())
+            .add(R.id.imageFragmentContainer1, fragment1)
+            .add(R.id.imageFragmentContainer2, fragment2)
             .commit()
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            fragment1.changeImage()
+            fragment2.changeImage()
+        }
     }
 }
