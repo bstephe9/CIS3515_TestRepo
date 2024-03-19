18117430ed8deb0a005f15a852fe11765e1a4807
bstephe9 <tun78906@temple.edu>
2024-03-18 20:05:28 -0400

    feat(ImageFragment): Retrieve imageView from layout, make it display random images on click

--- app/src/main/java/com/example/cis3515_testrepo/ImageFragment.kt
+++ app/src/main/java/com/example/cis3515_testrepo/ImageFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 
 class ImageFragment : Fragment() {
 
@@ -12,7 +13,25 @@ class ImageFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_image, container, false)
+
+        // Defining the array of images in the ImageFragment (probably better to pass it in)
+        val images = arrayOf(
+            R.drawable.a,
+            R.drawable.b,
+            R.drawable.c,
+        )
+
+        /* Retrieve the image view from the layout, setOnClickListener to display random image */
+//        // Inflate the layout for this fragment
+//        val layout = inflater.inflate(R.layout.fragment_image, container, false)
+//        val imageView = layout.findViewById<ImageView>(R.id.imageView)
+//        return layout
+
+        // Using scope functions instead
+        return inflater.inflate(R.layout.fragment_image, container, false).apply {
+            findViewById<ImageView>(R.id.imageView).setOnClickListener {
+                (it as ImageView).setImageResource(images.random())
+            }
+        }
     }
 }
