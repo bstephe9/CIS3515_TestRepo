efabd72de83366cfb48125d327d414a754a87377
bstephe9 <tun78906@temple.edu>
2024-03-25 21:43:48 -0400

    feat: Define scope for coroutine

--- app/src/main/java/com/example/cis3515_testrepo/MainActivity.kt
+++ app/src/main/java/com/example/cis3515_testrepo/MainActivity.kt
@@ -3,11 +3,22 @@ package com.example.cis3515_testrepo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /**
+         * Define scope for coroutine.
+         *
+         * For the scope's context, call the Job() constructor and specify that the Default
+         * dispatcher will execute this coroutine.
+         */
+        val scope = CoroutineScope(Job() + Dispatchers.Default)
 
         repeat(100) {
             Log.d("Countdown", (100 - it).toString())
