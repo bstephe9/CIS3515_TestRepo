7b84b7d0164b2680d39c92bfb835a4f45e22d793
bstephe9 <tun78906@temple.edu>
2024-03-25 21:42:12 -0400

    feat: Add countdown from 100

--- app/src/main/java/com/example/cis3515_testrepo/MainActivity.kt
+++ app/src/main/java/com/example/cis3515_testrepo/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.cis3515_testrepo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        repeat(100) {
+            Log.d("Countdown", (100 - it).toString())
+            Thread.sleep(1000)
+        }
     }
 }
