69e094a0a8b4c14c60a856fe8bc964fdbafc3d70
bstephe9 <tun78906@temple.edu>
2024-03-26 19:47:41 -0400

    feat: Use coroutine to extract HTML

--- app/src/main/java/com/example/cis3515_testrepo/MainActivity.kt
+++ app/src/main/java/com/example/cis3515_testrepo/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.cis3515_testrepo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /**
+         * Use a coroutine to extract HTML from wikipedia.
+         */
+        CoroutineScope(Dispatchers.IO).launch {
+            val url = URL("https://www.wikipedia.com")
+            val response = url.openStream().bufferedReader().readLine() // get the first line
+            Log.d("Response", response)
+        }
     }
 }
