7a92fcb3ea6cf56bcab7b77477822e4e1346797f
bstephe9 <tun78906@temple.edu>
2024-04-03 18:41:25 -0400

    feat(service): Override onStartCommand to run a timer

--- app/src/main/java/com/example/cis3515_testrepo/TimerService.kt
+++ app/src/main/java/com/example/cis3515_testrepo/TimerService.kt
@@ -3,10 +3,26 @@ package com.example.cis3515_testrepo
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import android.util.Log
 
 class TimerService : Service() {
 
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        runTimer()
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    fun runTimer() {
+        Thread {
+            for (i in 50 downTo 0) {
+                Log.d("Countdown", i.toString())
+                Thread.sleep(100)
+            }
+            stopSelf()
+        }.start()
     }
 }
