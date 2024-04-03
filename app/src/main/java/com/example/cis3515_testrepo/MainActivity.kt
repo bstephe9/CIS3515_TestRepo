1074f085dfe38431aa823905a91f66a32d99c068
bstephe9 <tun78906@temple.edu>
2024-04-03 18:44:30 -0400

    feat: Start service

--- app/src/main/java/com/example/cis3515_testrepo/MainActivity.kt
+++ app/src/main/java/com/example/cis3515_testrepo/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.cis3515_testrepo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            startService(Intent(this, TimerService::class.java))
+        }
     }
 }
