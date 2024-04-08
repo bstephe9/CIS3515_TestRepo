b651a5e1b4e12bfd20d6e0784d1cd4623c313322
bstephe9 <tun78906@temple.edu>
2024-04-08 19:54:47 -0400

    feat: Create ServiceConnection object

--- app/src/main/java/com/example/cis3515_testrepo/MainActivity.kt
+++ app/src/main/java/com/example/cis3515_testrepo/MainActivity.kt
@@ -1,17 +1,42 @@
 package com.example.cis3515_testrepo
 
+import android.content.ComponentName
 import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var timerBinder: TimerService.TimerBinder
+
+    /**
+     * Boolean to check if connected.
+     */
+    var isConnected = false
+
+    /**
+     * Create ServiceConnection object.
+     */
+    val serviceConnection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            timerBinder = service as TimerService.TimerBinder
+            isConnected = true
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            isConnected = false
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         findViewById<Button>(R.id.button).setOnClickListener {
-            startService(Intent(this, TimerService::class.java))
+
         }
     }
 }
