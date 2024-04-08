ed7edbb9aac0c8fee56798527354d04816a810bb
bstephe9 <tun78906@temple.edu>
2024-04-08 19:55:36 -0400

    feat: Start timer when button clicked

--- app/src/main/java/com/example/cis3515_testrepo/MainActivity.kt
+++ app/src/main/java/com/example/cis3515_testrepo/MainActivity.kt
@@ -42,7 +42,7 @@ class MainActivity : AppCompatActivity() {
         )
 
         findViewById<Button>(R.id.button).setOnClickListener {
-
+            if (isConnected) timerBinder.startTimer()
         }
     }
 
