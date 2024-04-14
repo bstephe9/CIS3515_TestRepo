df4dc60d7940a53b67ebf6dba9b28a6c7bc3ceb7
bstephe9 <tun78906@temple.edu>
2024-04-14 14:26:20 -0400

    feat: Inflate resource into menu object

--- app/src/main/java/com/example/cis3515_testrepo/MainActivity.kt
+++ app/src/main/java/com/example/cis3515_testrepo/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.cis3515_testrepo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        // Inflate resource into the menu object.
+        menuInflater.inflate(R.menu.main, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
